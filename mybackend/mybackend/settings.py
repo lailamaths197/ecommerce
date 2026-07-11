4f486052c974d3ef6efadf027431a2b5a75cf909
lailamaths197 <lailamaths197@gmail.com>
2026-07-11 08:44:46 +0530

    Deploy: Permitted production frontend URL in CORS configuration

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://onrender.com"
+    "https://ecommerce-frontend-v6yb.onrender.com"
 ]
 
 LANGUAGE_CODE = 'en-us'
